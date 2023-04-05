83aff3641a1c6c845b8200cd8ae8f96041e4272a
progall <progall@users.noreply.github.com>
2023-04-06 00:27:43 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import math
 
 chat_id = 322172960 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +8,9 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    
+    for each i in x:
+        {i = i/10-37+ math.exp(1)
+        }
+    
     return x.mean() # Ваш ответ
